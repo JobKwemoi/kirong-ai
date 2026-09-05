ad9087b0dfa9a3ec4905b0ca863b43d7d67205cf
JobKwemoi <kirongjob@gmail.com>
2026-09-05 13:37:32 +0300

    Replace handler with vercelHandler in user function

--- netlify/functions/user.js
+++ netlify/functions/user.js
@@ -1,9 +1,6 @@
-import handler from "../../api/user.js";
+import vercelHandler from "../../api/user.js";
 import { runVercelHandler } from "../lib/vercel-bridge.js";
 
 export async function handler(event) {
-  return runVercelHandler(
-    event,
-    handler
-  );
+  return runVercelHandler(event, vercelHandler);
 }
